cf863be56eaf01379508a48d03e8abd14bbf5c5f
Shamil <badalloffsh@gmail.com>
2018-06-26 18:46:43 +0200

    Refactor: totalPayload presenter

--- presenters/costs_by_year.rb
+++ presenters/costs_by_year.rb
@@ -5,7 +5,11 @@ module Presenters
     end
 
     def print
-
+      binding.pry
+      hsh = launches.group_by { |h| Date.parse h[:date] }.map do |k,v|
+        {:created => k.to_s,:cost => v.map {|h1| h1[:cost]}.inject(:+)}
+      end
+      hsh
     end
   end
 end
